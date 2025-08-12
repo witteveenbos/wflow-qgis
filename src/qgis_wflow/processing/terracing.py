663f4ff08f72b5ea6c7b6905ddd228ab52cfc8f9
Anne den Hartog <anne.den.hartog@witteveenbos.com>
2025-08-12 09:16:21 +0200

    implement the terracing changes to the slope layer

--- src/qgis_wflow/processing/terracing.py
+++ src/qgis_wflow/processing/terracing.py
@@ -3,6 +3,8 @@ import shutil
 import subprocess
 import typing
 from pathlib import Path
+import xarray as xr
+import rioxarray
 
 from qgis import processing
 from qgis.core import (
@@ -53,6 +55,7 @@ class ApplyTerracingAlgorithm(AlgorithmBase):
                 self.tr('Original slope layer')
             )
         )
+        # Vector layer that gives the areas where the slope should be adjusted
         self.addParameter(
             QgsProcessingParameterFeatureSource(
                 self.TERRACING_VECTOR,
@@ -61,8 +64,7 @@ class ApplyTerracingAlgorithm(AlgorithmBase):
                 optional=True
             )
         )
-        # @Anne: Maybe Daan wants to configute the model with a field that contains the slope value
-        # which should be used for terracing.
+       
         self.addParameter(
         QgsProcessingParameterNumber(
                 name=self.TERRACING_FIELD,
@@ -72,16 +74,8 @@ class ApplyTerracingAlgorithm(AlgorithmBase):
                 optional=True
             )
         )
-# QgsProcessingParameterField(
-            #     name=self.TERRACING_FIELD,
-            #     description=self.tr('Field with terracing values'),
-            #     parentLayerParameterName=self.TERRACING_VECTOR,
-            #     type=QgsProcessingParameterField.Numeric,
-            #     optional=True
-            # )
 
-        # For now we just add a file destination parameter. Can be nicer when users can select a destination folder
-        # and the name of the file is automatically generated (or the name is a parameter as well).
+        # Destination folder for the updated wflow model
         self.addParameter(
             QgsProcessingParameterFolderDestination(
                 self.TARGET,
@@ -114,19 +108,8 @@ class ApplyTerracingAlgorithm(AlgorithmBase):
             feedback.reportError("Failed to import required libraries. Please run installer (Plugins->WFlow->Configuration)")
             return {}
         
-        # # Get the folder to copy the wflow model into
-        # base_path = Path(parameters[self.TARGET])
 
-        # # Copy the input file to the target location
-        # base_raster = self.parameterAsRasterLayer(parameters, self.BASE, context)
-        # base_raster_path = Path(base_raster.dataProvider().dataSourceUri())
-        # target_raster_path = base_path / 'shapes' / f'{base_raster_path.stem}_update_landuse{base_raster_path.suffix}'
-        # if not target_raster_path.parent.exists():
-        #     target_raster_path.parent.mkdir(parents=True, exist_ok=True)
-        # feedback.pushInfo(f"Copying {base_raster_path} to {base_path / 'shapes' / f'{base_raster_path.stem}_update_landuse{base_raster_path.suffix}'}")
-        # shutil.copy2(base_raster_path, target_raster_path)
-
-        # Get the input folder and target folder paths
+        # Get the input and target folder from the parameters
         input_folder = Path(parameters[self.INPUT])
         target_folder = Path(parameters[self.TARGET])
 
@@ -136,97 +119,78 @@ class ApplyTerracingAlgorithm(AlgorithmBase):
         shutil.copytree(input_folder, target_folder)
 
         feedback.pushInfo(f"Copied folder {input_folder} to {target_folder}")
+
+        # Find and adjust the .toml file in the target folder
         for file in target_folder.iterdir():
             if file.suffix == ".toml":
                 new_file = file.with_name(f"{file.stem}_with_terracing{file.suffix}") 
                 file.rename(new_file)
 
-        # @Anne: The code below is the work-horse of the algorithm. It will update the slope layer (a raster layer) with
-        # the values from the vector layer. This code should be adapted to work with the terracing vector layer. If Daan
-        # wants to use a fixed value for terracing, you have to change the algorithm 'gdal:rasterize_over' to 
-        # 'gdal:rasterize_over_fixed_value'. You can run the algorithm in QGIS and select the Advanced (bottom left corner)
-        # and then Copy as Python Command. This will give you the code to use in the algorithm.
+                with open(new_file, "r") as f:
+                    lines = f.readlines()
+                with open(new_file, "w") as f:
+                    for line in lines:
+                        if line.strip().startswith("path_static"):
+                            f.write('path_static = "staticmaps_with_terracing.nc"\n')
+                        elif line.strip().startswith("casename ="):
+                            f.write('casename = "wflow_sbm_with_terracing"\n')
+                        else:
+                            f.write(line)
+
+        # create mask from vector layer. For the raster calculator to work we need a rasterized mask of the terracing vector layer
         base_raster = self.parameterAsRasterLayer(parameters, self.BASE, context)
         base_raster_path = base_raster.dataProvider().dataSourceUri()
+        feedback.pushInfo(f"Raster width: {base_raster.width()}, height: {base_raster.height()}, extent: {base_raster.extent()}")
+        # change vector layer to raster layer with the terracing values
+        mask_raster = target_folder / "terracing_mask.tif"
+        processing.run(
+            "gdal:rasterize",
+            {
+                'INPUT': parameters[self.TERRACING_VECTOR],
+                'FIELD': None, 
+                'BURN': 1,
+                'UNITS': 0,
+                'WIDTH': base_raster.width(),
+                'HEIGHT': base_raster.height(),
+                'EXTENT': base_raster.extent(),
+                'NODATA': -999,
+                'DATA_TYPE': 5,  # Float32
+                'INIT': 0,
+                'OUTPUT': str(mask_raster)
+            },
+            context=context,
+            feedback=feedback,
+            is_child_algorithm=True
+        )
+
+        # given the mask raster and the base raster, create a new raster with the adjusted terracing values
+        # hardcoded that the target rasterpath is put into staticmaps.nc":Slope 
+        feedback.pushInfo(f"Base raster path: {base_raster_path}")
+        orig_nc = target_folder / "staticmaps.nc"
+        output_nc = target_folder / "staticmaps_with_terracing.tif"
         if parameters[self.TERRACING_VECTOR] is not None:
-                processing.run(
+               algresult = processing.run(
                     "gdal:rastercalculator", 
                     {
                         'INPUT_A':base_raster_path,
                         'BAND_A':1,
-                        'FORMULA':f'A*{parameters[self.TERRACING_FIELD]}',
-                        'EXTENT_OPT':2,
+                        'INPUT_B': str(mask_raster),
+                        'BAND_B': 1,
+                        'FORMULA': f'A*(1-B)+A*B*{parameters[self.TERRACING_FIELD]}',
                         'RTYPE':5,
-                        'OUTPUT':'TEMPORARY_OUTPUT'
+                        'OUTPUT': str(output_nc)
                     },
                     context=context,
                     feedback=feedback,
                     is_child_algorithm=True
                 )
-        # if parameters[self.LAND_USE_VECTOR] is not None:
-        #     algresult = processing.run(
-        #         "gdal:rasterize_over",
-        #         {
-        #             'INPUT' : parameters[self.TERRACING_VECTOR],
-        #             'FIELD' : parameters[self.TERRACING_FIELD],
-        #             'INPUT_RASTER' : str(target_raster_path),
-        #             'ADD' : False,
-        #             'EXTRA' : '',
-        #         },
-        #         context=context,
-        #         feedback=feedback,
-        #         is_child_algorithm=True
-        #     )
-
-        # Create the required files
-        # @Anne: the code below is not yet implemented. It should create the ini and yml files required by hydromt_wflow.
-        #        You can ask Daan what the contents of these files should be.
-        # ini_file = target_folder / "build_apply_terracing.ini"
-        # with open(ini_file, "w") as f:
-        #     f.write("[setup_lulcmaps]\n")
-        #     f.write(f"lulc_fn         = {LULC_MAPS[parameters[self.LULC_MAP]]}          # source for lulc maps: {{globcover, vito, corine}})\n")
-        # yml_file = target_folder / "data_catalog_apply_terracing.yml"
-        # with open(yml_file, "w") as f:
-        #     lines = [
-        #         f"root: {str(target_folder)}\n",
-        #         "meta:\n",
-        #         "  version: '2023.11'\n",
-        #         "\n",
-        #         f"{LULC_MAPS[parameters[self.LULC_MAP]]}:\n",
-        #         "  crs: 4326\n",
-        #         "  data_type: RasterDataset\n",
-        #         "  driver: raster\n",
-        #         "  filesystem: local\n",
-        #         f"  path: ./shapes/{target_raster_path.name}\n",
-        #         "  meta:\n",
-        #         *LULC_META[LULC_MAPS[parameters[self.LULC_MAP]]]
-        #     ]
-        #     f.writelines(lines)
-   
-        # # Run the hydromt command to update the land use map -> taken from land use. in this case should alter the slope layer in the staticmaps.nc 
-        # process = subprocess.Popen(
-        #     [
-        #         Path(hydromt_wflow.__file__).parent.parent.parent / "Scripts" / "hydromt.exe",
-        #         "update",
-        #         "wflow",
-        #         str(Path(parameters[self.INPUT]).parent),
-        #         "-o", str(base_path),
-        #         "-i", str(ini_file),
-        #         "-d", str(yml_file),
-        #         "-vvv",
-        #     ],
-        #     stdout=subprocess.PIPE,
-        #     stderr=subprocess.STDOUT,
-        #     shell=True,
-        #     encoding='utf-8',
-        #     errors='replace'
-        # )
-        # while (realtime_output := process.stdout.readline()) != '' or process.poll() is None:
-        #     if realtime_output:
-        #         match = self.PROGRESS_REGEX.search(realtime_output)
-        #         if match:
-        #             feedback.setProgress(int(match.group(1)))
-        #         else:
-        #             feedback.pushInfo(realtime_output.strip())
+        
+        # Open the original netCDF file and add the new slope layer
+        ds = xr.open_dataset(orig_nc)
+        new_slope = rioxarray.open_rasterio(str(output_nc)).squeeze()
+        ds['Slope'] = new_slope
+        out_nc = target_folder / "staticmaps_with_terracing.nc"
+        ds.to_netcdf(out_nc)
+        ds.close()
 
         return {}

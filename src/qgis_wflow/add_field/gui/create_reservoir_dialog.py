e1495cdb474531adabd5b20e04233215d578c116
Vincent van Duijnhoven <vincent.van.duijnhoven@witteveenbos.com>
2026-08-07 11:10:22 +0200

    velden aangepast

--- src/qgis_wflow/add_field/gui/create_reservoir_dialog.py
+++ src/qgis_wflow/add_field/gui/create_reservoir_dialog.py
@@ -71,8 +71,8 @@ class CreateReservoir(QDialog):
             QgsField("reservoir_demand", QMetaType.Double),
             QgsField("reservoir_max_release", QMetaType.Double),
             QgsField("reservoir_target_full_fraction", QMetaType.Double),
-            QgsField("Dam_height", QMetaType.Double),
-            QgsField("Depth_avg", QMetaType.Double),
+            QgsField("reservoir_rating_curve", QMetaType.Double),
+            QgsField("reservoir_storage_curve", QMetaType.Double),
             ])
         reservoir_layer.commitChanges()
         
